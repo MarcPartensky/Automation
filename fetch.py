e261a04c76baefb4541486b187834684d1fce783
marc <marc.partensky@gmail.com>
2021-02-14 23:48:09 +0100

    share and fetch done

---
+++ fetch.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+"""Fetch a file on the website of Marc Partensky."""
+
+import os
+import requests
+import argparse
+import yaml
+import pyperclip
+
+from rich import print
+
+CONFIG_PATH = os.path.abspath(
+    os.path.join(os.environ["PROGRAMS_PATH"], "automation", "share.yml")
+)
+
+with open(CONFIG_PATH, "r") as file:
+    config = yaml.safe_load(file)
+
+parser = argparse.ArgumentParser(description=__doc__)
+
+parser.add_argument("url", help="url to fetch")
+parser.add_argument("filename", nargs="?", help="filename to download to")
+
+parser.add_argument(
+    "-t",
+    "--token",
+    nargs="?",
+    default=config["token"],
+    help="token for authentication",
+)
+
+args = parser.parse_args()
+
+
+def fetch(url: str, token: str = None, filename: str = None):
+    """Fetch a file with restricted access."""
+    session = requests.Session()
+    response = session.get(url, params=dict(token=token), allow_redirects=True)
+
+    print(response)
+    if response.status_code != 200:
+        text = "".join(("[bold red]", response.reason, "[/bold red]: ", response.text))
+        print(text)
+        return response
+
+    if not filename:
+        filename = (
+            response.headers["Content-Disposition"]
+            .split(";")[1]
+            .strip()
+            .replace("filename=", "")
+            .replace('"', "")
+        )
+
+    path = os.path.join(os.getcwd(), filename)
+
+    with open(path, "w") as f:
+        f.write(response.text)
+
+    pyperclip.copy(path)
+
+    print(
+        "[bold green]Success[/bold green]: "
+        f"downloaded to '[italic magenta]{path}[/italic magenta]' "
+        "and [bold]copied path[/bold] to clipboard."
+    )
+
+
+if __name__ == "__main__":
+    fetch(url=args.url, token=args.token, filename=args.filename)
--- share.py
+++ share.py
@@ -11,6 +11,7 @@ import argparse
 import requests
 import logging
 import yaml
+import pyperclip
 
 from rich import print
 
@@ -33,6 +34,14 @@ parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument("file", help="file to share.")
 
 parser.add_argument(
+    "-t",
+    "--token",
+    nargs="?",
+    default=config["token"],
+    help="token for authentication",
+)
+
+parser.add_argument(
     "-u", "--url", dest="urls", nargs="+", default=config["url"], help="website url"
 )
 
@@ -40,24 +49,43 @@ parser.add_argument(
     "-m", "--method", nargs="?", default=config["method"], help="http method"
 )
 
-parser.add_argument("-v", "--vars", nargs="+", help="http variables (post/get/...)")
+parser.add_argument(
+    "-p", "--public", nargs="?", default=config["public"], help="http method"
+)
 
-parser.add_argument("-c", "--config", default=config, help="default config filepath.")
+parser.add_argument(
+    "--perm",
+    "--permission",
+    dest="permissions",
+    nargs="*",
+    default=config["permission"],
+    help="http method",
+)
+# parser.add_argument("-c", "--config", default=config, help="default config filepath.")
+
+parser.add_argument(
+    "-c", "--cert", nargs="?", default=config["cert"], help="tls/ssl certificate path"
+)
 
 
 def share_file(
-    file: str, urls: list = [], method: str = "POST", vars: list = [], config: dict = {}
+    file: str,
+    urls: list = [],
+    method: str = "POST",
+    token: str = None,
+    cert: str = None,
+    public: bool = None,
 ):
     """Share a file using a website."""
-    path = file.replace("./", "")
-    file = path.split("/")[-1]
+    path = os.path.abspath(file)
+    file = file.replace("./", "").split("/")[-1]
 
-    with open(path, "r") as f:
-        content = f.readlines()
+    with open(path, "rb") as f:
+        content = f.read()
 
     # check for empty file
-    content = "\n".join(content[1:]).strip()
-    if content == "":
+    # content = "\n".join(content[1:]).strip()
+    if content == b"":
         raise Exception("This file is empty!")
 
     raw_files = {"file": (file, content)}
@@ -67,11 +95,39 @@ def share_file(
         url = url.replace("http://", "").replace("https://", "")
         logging.info(f"Sending file to {url}.")
 
-        response = session.request(method=method, url=url, params=vars, files=raw_files)
+        if cert:
+            protocol = "https"
+        else:
+            protocol = "http"
 
+        full_url = f"{protocol}://{url}/f"
+
+        response = session.post(
+            url=full_url,
+            data=dict(token=token, public=public),
+            files=raw_files,
+            # cert=cert,
+            allow_redirects=True,
+            cookies={"from-my": "browser"},
+        )
+
+        url = f"{protocol}://{response.text}"
+
+        pyperclip.copy(url)
+
+        print("[bold green]Success[/bold green]: [bold]copied url[/bold] to clipboard.")
+        print(url)
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
     logging.info(args)
-    share_file(args.file, args.urls, args.method, args.vars, args.config)
+    share_file(
+        file=args.file,
+        urls=args.urls,
+        method=args.method,
+        token=args.token,
+        cert=args.cert,
+        # expiration=args.expiration,
+        public=args.public,
+    )
