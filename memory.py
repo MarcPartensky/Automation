54e5b161b482088223629bd247f3947ca3efdb61
marc <marc.partensky@gmail.com>
2021-06-26 15:34:14 +0200

    added new dependencies

---
+++ memory.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+"""Helps you remember things regularly.
+
+Uses notifications to do so.
+"""
+
+import os
+import logging
+import click
+import yaml
+
+from rich import print
+
+from notifypy import Notify
+
+
+def notify(message: str):
+    """Notify with a given message."""
+    notification = Notify(
+        default_notification_title="Remember",
+        default_application_name="name",
+        default_notification_icon=http_path,
+        # default_notification_audio=""
+    )
+
+    notification.message = message
+    notification.send()
+
+
+@click.group()
+def mem():
+    pass
+
+
+@mem.command()
+@mem.argument("item", type=str, multiple=True, help="item to remember")
+def add(item: str):
+    """Add an item to remember."""
+
+    click.echo("item added")
+
+
+@mem.command()
+@mem.argument("item", type=str, multiple=True, help="item to remember")
+def delete():
+    """Delete an item to remember."""
+    click.echo("delete")
+
+
+@mem.command(name="notify")
+def notify_():
+    """Notify with an item to remember"""
+    notify("test")
+    click.echo("notify")
+
+
+@mem.command(name="list")
+def list_():
+    """List items to remember"""
+    click.echo("notify")
+
+
+# def remember(args: argparse.Namespace):
+#     """Remember a new item."""
+
+#     with open("remember.yml", "r") as f:
+#         content = yaml.safe_load(f)
+
+#     print(args)
+
+#     if not content:
+#         content = {}
+
+#     if not "list" in content:
+#         content["list"] = []
+
+#     if not "current" in content:
+#         content["current"] = 0
+
+#     if args.add:
+#         content["list"].append(args.add)
+
+#     if args.delete:
+#         del content["list"][args.delete]
+
+#     elif args.next:
+#         current = content["current"]
+#         print(content["list"][current])
+#         content["current"] = (current + 1) % len(content["list"])
+#         notify(content["list"][current])
+
+#     elif args.list:
+#         print(list(enumerate(content["list"])))
+
+#     with open("remember.yml", "w") as f:
+#         yaml.dump(content, f)
+
+
+# if __name__ == "__main__":
+#     if not os.path.exists("remember.yml"):
+#         print("No [bold]remember.yml[/bold] file found. Creating one instead.")
+#         open("remember.yml").close()
+#     args = parser.parse_args()
+#     remember(args)
+if __name__ == "__main__":
+    mem()
